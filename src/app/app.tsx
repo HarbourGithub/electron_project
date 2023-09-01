17a3c98e39f3c74544684c665cb2ac4b3665759c
hgwang <hgwang@grandstream.cn>
2023-09-01 20:26:47 +0800

    [NBF] add code Cause: None Solution: None Fixed Version: None Branch: master

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+function App() {
     return (
         <div>
             <h1>App</h1>
---
+++ src/components/desktopHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DesktopHeader() {
+    return (
+        <div>
+            <h1>DesktopHeader</h1>
+        </div>
+    )
+}
+
+export default DesktopHeader
